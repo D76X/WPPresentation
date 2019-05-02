ff266bc3fbdfc09ac9c2908ecfe2e9643c0b9307
Davide Spano <davide.spano@wuerth-phoenix.com>
2019-05-02 17:28:47 +0200

    mods

--- Apps/01KeyVaultApp/Program.cs
+++ Apps/01KeyVaultApp/Program.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Microsoft.Azure.KeyVault;
+using System.Threading.Tasks;
+using System.Net.Http;
 //using AzureResourceReport.Models;
 //using Microsoft.Azure.Management.ResourceManager.Fluent;
 //using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
@@ -14,15 +17,115 @@ namespace _01KeyVaultApp
     // https://azurecto.com/azure-keyvault-authenticating-with-certificates-and-reading-secrets/
     // https://stackoverflow.com/questions/47875589/cant-access-azure-key-vault-from-desktop-console-app
     // https://docs.microsoft.com/en-us/azure/key-vault/tutorial-net-create-vault-azure-web-app
+    // https://github.com/damienbod/AspNetCoreBackChannelLogout/blob/master/ConsoleStandaloneUsingAzureSecrets/Program.cs
+
+    //{
+    //"DNSNameKeyVault": "<DNS NAME from Key Vault>",
+    //"AADAppRegistrationAppId": "<Azure AD App Registration Application ID>",
+    //"AADAppRegistrationAppSecret": "<Azure AD App Registration Application Secret>",
+    //"SomeSecret": "DEV_VALUE"
+    //}
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("Start Application and get key vault values");
 
-            
+            // https://stackoverflow.com/questions/1189364/reading-settings-from-app-config-or-web-config-in-net
+            string config_kv_dnsname = ConfigurationManager.AppSettings["kv_dnsname"];
+            string config_kv_appId = ConfigurationManager.AppSettings["kv_appId"];
+            string config_kv_key = ConfigurationManager.AppSettings["kv_key"];
+
+            Console.WriteLine($"{config_kv_dnsname}");
+            Console.WriteLine($"{config_kv_appId}");
+            Console.WriteLine($"{config_kv_key}");
+
+            // https://docs.microsoft.com/en-us/dotnet/api/overview/azure/key-vault?view=azure-dotnet
+            //var keyClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(securityToken));
+            //await GetSecretAsync("https://YOURVAULTNAME.vault.azure.net/", "YourSecretKey");
+
+            Console.ReadKey();
+        }
+
+        // https://stackoverflow.com/questions/47875589/cant-access-azure-key-vault-from-desktop-console-app
+        private static async Task<string> GetSecretAsync(string vaultUrl, string vaultKey)
+        {
+            var client = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(GetAccessTokenAsync), new HttpClient());
+            var secret = await client.GetSecretAsync(vaultUrl, vaultKey);
+
+            return secret.Value;
+        }
+
+        private static async Task<string> GetAccessTokenAsync(string authority, string resource, string scope)
+        {
+            //DEMO ONLY
+            //Storing ApplicationId and Key in code is bad idea :)
+            var appCredentials = new ClientCredential("YourApplicationId", "YourApplicationKey");
+            var context = new AuthenticationContext(authority, TokenCache.DefaultShared);
+
+            var result = await context.AcquireTokenAsync(resource, appCredentials);
+
+            return result.AccessToken;
         }
     }
 }
+
+//--------------------------------------------------------------------------------------
+// Application Registration
+// https://damienbod.com/2019/02/07/using-azure-key-vault-from-an-non-azure-app/
+// https://stackoverflow.com/questions/47875589/cant-access-azure-key-vault-from-desktop-console-app
+// 01KeyVaultApp
+// Application(client) ID: 3da2ec55-4670-4e30-b7f2-6c4a29ec9a3f
+// Directory(tenant) ID : 981b07d1-b261-4c3e-a400-b86f7809d9bc
+// Object ID : 2458fef3-3247-4fb2-a3ab-e144b93d1818
+// Supported account types : My organization only
+// Redirect URIs :Add a Redirect URI
+// Managed application in local directory :01KeyVaultApp
+// --------------------------------------------------------------------------------------
+// https://damienbod.com/2019/02/07/using-azure-key-vault-from-an-non-azure-app/
+// Now a secret for the AAD Application registration needs to be created.
+// Click the Certificates & secrets button, and then New client secret.
+// Store the client secret!
+// /nKN++8?2m2LUUTdWva5bwcZHR__slTr
+// /nKN++8?2m2LUUTdWva5bwcZHR__slTr
+// --------------------------------------------------------------------------------------
+// Secrets vs Certificates for Client App Registration
+// Credentials enable applications to identify themselves to the authentication service when receiving tokens at a web addressable location(using an HTTPS scheme).
+// For a higher level of assurance, we recommend using a certificate(instead of a client secret) as a credential.
+//--------------------------------------------------------------------------------------
+
+// https://www.rahulpnath.com/blog/pfx-certificate-in-azure-key-vault/
+//// Client
+//byte[] encryptedData;
+//// You can also use the PFX here as it contains the private key
+//var publicCertificate = new X509Certificate2(@"C:\CertificateKey.cer"); 
+//using (var cryptoProvider = publicCertificate.PublicKey.Key as RSACryptoServiceProvider)
+//{
+//var byteData = Encoding.Unicode.GetBytes(textToEncrypt);
+//encryptedData = cryptoProvider.Encrypt(byteData, true);
+//}
+
+////Server
+//var privateCertificate = new X509Certificate2(@"C:\CertificateKey.pfx", "test");
+//using (var cryptoProvider = privateCertificate.PrivateKey as RSACryptoServiceProvider)
+//{
+//var decryptedData = cryptoProvider.Decrypt(encryptedData, true);
+//var decryptedText = Encoding.Unicode.GetString(decryptedData);
+//}
+//-----------------
+//var keyIdentifier = "https://rahulkeyvault.vault.azure.net:443/keys/KeyFromCert/";
+
+//// Client Remains the same or use the Key Vault Client
+//var encryptedResult = await keyClient.EncryptAsync(keyIdentifier, "RSA-OAEP", byteData);
+
+//// Server
+//var decryptedData = await keyClient.DecryptAsync(keyIdentifier, "RSA-OAEP", certED);
+//var decryptedText = Encoding.Unicode.GetString(decryptedData.Result);
+//-------------
+//var secretRetrieved = await keyClient.GetSecretAsync(secretIdentifier);
+//var pfxBytes = Convert.FromBase64String(secretRetrieved.Value);
+//File.WriteAllBytes(@"C:\cert\ADTestVaultApplicationNew.pfx", pfxBytes);
+
+//// or recreate the certificate directly
+//var certificate = new X509Certificate2(pfxBytes);
