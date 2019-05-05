6763cf53aff33ac46a0844dcc80088095de16850
D76X <davide.spano.x@gmail.com>
2019-05-05 16:26:49 +0200

    more scripts

--- Apps/01AlwaysEncryptedApp/Program.cs
+++ Apps/01AlwaysEncryptedApp/Program.cs
@@ -1,63 +1,53 @@
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.SqlServer.Management.AlwaysEncrypted.AzureKeyVaultProvider;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Net.Http;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
-using Microsoft.Azure.KeyVault;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
-
-namespace _03KeyVaultApp
+namespace _01AlwaysEncryptedApp
 {
     class Program
     {
-        // the ID assigned to this app when registered an Azure ID
-        // In this version of teh app the is no secret as the app authenticate
-        // on Azure AD by means of a certificate installed on the client machine
-        // therefore avoiding any client secret exchange and the need to protect
-        // the secret locally. You still have the thumbprint of the certificate
-        // in the config file but that is NOT sensitive information as it is a 
-        // mear hash of teh certificate and cannot be used by attackers.
         private static string AppId;
         private static string PfxThumbprint;
-
-        // the URI to the Azure Key Vault that holds the Secret, Key or Certificate
-        // that the application needs to retrieve
-        private static string VaultResourceUri;
+        private static string AccessToken;
 
         static async Task Main(string[] args)
         {
             Console.WriteLine($"Start Application {System.Reflection.Assembly.GetExecutingAssembly().GetName().Name} and get key vault values");
-            
+
             string config_aad_appId = ConfigurationManager.AppSettings["aad_appId"];
             string config_pfxthumbprint = ConfigurationManager.AppSettings["pfxthumbprint"];
-            string config_kv_secret_uri = ConfigurationManager.AppSettings["kv_secret_uri"];
-            string config_kv_secret_uri_versioned = ConfigurationManager.AppSettings["kv_secret_uri_versioned"];
+            //string config_kv_secret_uri = ConfigurationManager.AppSettings["kv_secret_uri"];
+            //string config_kv_secret_uri_versioned = ConfigurationManager.AppSettings["kv_secret_uri_versioned"];
             string config_kv_dnsname = ConfigurationManager.AppSettings["kv_dnsname"];
+            string connectionString = ConfigurationManager.AppSettings["connectionString"];
+            
+            Console.WriteLine();
 
             Console.WriteLine();
             Console.WriteLine($"{nameof(config_aad_appId)}={config_aad_appId}");
             Console.WriteLine($"{nameof(config_pfxthumbprint)}={config_pfxthumbprint}");
-            Console.WriteLine($"{nameof(config_kv_secret_uri)}={config_kv_secret_uri}");
-            Console.WriteLine($"{nameof(config_kv_secret_uri_versioned)}={config_kv_secret_uri_versioned}");
+            //Console.WriteLine($"{nameof(config_kv_secret_uri)}={config_kv_secret_uri}");
+            //Console.WriteLine($"{nameof(config_kv_secret_uri_versioned)}={config_kv_secret_uri_versioned}");
             Console.WriteLine($"{nameof(config_kv_dnsname)}={config_kv_dnsname}");
-            Console.WriteLine();            
-
-            AppId = config_aad_appId;
-            PfxThumbprint = config_pfxthumbprint;
-            VaultResourceUri = config_kv_secret_uri;
-
-            var secret = await GetSecretAsync(secretUri: VaultResourceUri);
-            Console.WriteLine($"{nameof(secret)}={secret}");
-            Console.WriteLine();
-
-            string connectionString = secret;
             Console.WriteLine($"{nameof(connectionString)}={connectionString}");
             Console.WriteLine();
 
+            AppId = config_aad_appId;
+            PfxThumbprint = config_pfxthumbprint;
+
+            var azEncryptionKeyVaultProvider = new SqlColumnEncryptionAzureKeyVaultProvider(GetAccessTokenAsync);
+            var providers = new Dictionary<string, SqlColumnEncryptionKeyStoreProvider>();
+            providers.Add(SqlColumnEncryptionAzureKeyVaultProvider.ProviderName, azEncryptionKeyVaultProvider);
+            SqlConnection.RegisterColumnEncryptionKeyStoreProviders(providers);
+            Console.WriteLine($"providers = {providers.Count}");            
+            Console.WriteLine();
+            
             var results = ReadData(connectionString);
             results.ToList().ForEach(Console.WriteLine);
             Console.WriteLine();
@@ -92,38 +82,6 @@ namespace _03KeyVaultApp
             return result;
         }
 
-        private static async Task<string> GetSecretAsync(string secretUri)
-        {
-            var client = new KeyVaultClient(
-                authenticationCallback: new KeyVaultClient.AuthenticationCallback(GetAccessTokenAsync),
-                httpClient: new HttpClient());
-
-            var secret = await client.GetSecretAsync(secretIdentifier: secretUri);
-
-            return secret.Value;
-        }
-
-        /// <summary>
-        /// Retrieves a x509 from the local store if any installed certificates
-        /// matches the given thumbprint.
-        /// </summary>
-        /// <param name="pfxthumbprint">the thumbprint of a PFX</param>
-        /// <returns></returns>
-        private static X509Certificate2 GetCertificate(string pfxthumbprint)
-        {
-            X509Store certStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
-            certStore.Open(OpenFlags.ReadOnly);
-            X509Certificate2Collection certCollection = certStore.Certificates.Find(
-                                 X509FindType.FindByThumbprint,
-                                 pfxthumbprint,
-                                 false);
-            X509Certificate2 cert = certCollection[0];
-            Console.WriteLine($"certificate name = {cert.FriendlyName}");
-            Console.WriteLine();
-            certStore.Close();
-            return cert;
-        }
-
         /// <summary>
         /// Authenticates with Azure AD and retrieves an access token 
         /// for the resource that this application needs to access.
@@ -146,9 +104,46 @@ namespace _03KeyVaultApp
                 clientCertificate: clientAssertionCertificate);
 
             var accessToken = result.AccessToken;
-            Console.WriteLine($"{nameof(accessToken)}={accessToken}");
-            Console.WriteLine();
+            SetAccessToken(accessToken);
+            //Console.WriteLine($"{nameof(accessToken)}={accessToken}");
+            //Console.WriteLine();
             return result.AccessToken;
+        }
+
+        private static void SetAccessToken(string accessToken)
+        {
+            if (string.IsNullOrEmpty(AccessToken))
+            {
+                AccessToken = accessToken;
+                Console.WriteLine($"{nameof(AccessToken)}={AccessToken}");
+                Console.WriteLine();
+                return;
+            }
+            else if(AccessToken != accessToken)
+            {
+                throw new InvalidOperationException("why new access token?");
+            }            
+        }
+
+        /// <summary>
+        /// Retrieves a x509 from the local store if any installed certificates
+        /// matches the given thumbprint.
+        /// </summary>
+        /// <param name="pfxthumbprint">the thumbprint of a PFX</param>
+        /// <returns></returns>
+        private static X509Certificate2 GetCertificate(string pfxthumbprint)
+        {
+            X509Store certStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            certStore.Open(OpenFlags.ReadOnly);
+            X509Certificate2Collection certCollection = certStore.Certificates.Find(
+                                 X509FindType.FindByThumbprint,
+                                 pfxthumbprint,
+                                 false);
+            X509Certificate2 cert = certCollection[0];
+            Console.WriteLine($"certificate name = {cert.FriendlyName}");
+            Console.WriteLine();
+            certStore.Close();
+            return cert;
         }
     }
 }
