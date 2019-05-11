55a271a580130f49fc6f259ea86f2dbbb464cd19
D76X <davide.spano.x@gmail.com>
2019-05-11 12:17:57 +0200

    more notes

---
+++ Powershell/13.CreateAppServicePlan&WebAppServiceApp.ps1
@@ -0,0 +1,26 @@
+﻿# ----------------------------------------------------------------------------------------------
+# Create an Azure App Service Web App
+# Refs
+# https://blogs.msdn.microsoft.com/benjaminperkins/2017/10/02/create-an-azure-app-service-web-app-using-powershell/
+# https://blogs.msdn.microsoft.com/benjaminperkins/2014/10/01/azure-website-hosting-plans-whp/
+# https://www.youtube.com/watch?v=jCfJWYocz1M
+# ----------------------------------------------------------------------------------------------
+Login-AzureRmAccount
+Set-AzureRmContext -Subscription "df17c9fe-de76-4143-bbae-77b75fa0705b"
+
+$resourceGroupName = "wppres1rg1"
+$location = "West Europe"          
+$appServicePlanName = "wpserviceplan1"
+$appServiceName ="04WebApp"
+
+New-AzureRmAppServicePlan -ResourceGroupName $resourceGroupName `
+-Location $location `
+-Tier Free `
+-Name $appServicePlanName
+
+New-AzureRmWebApp -ResourceGroupName $resourceGroupName `
+-Location $location `
+-AppServicePlan $appServicePlanName `
+-Name $appServiceName `
+
+Get-AzureRmWebApp
