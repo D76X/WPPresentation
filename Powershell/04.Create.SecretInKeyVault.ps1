b623d583ac5b30bbebb2a76f5abcfcb522fadd01
D76X <davide.spano.x@gmail.com>
2019-05-19 18:47:45 +0200

    more code

--- Powershell/04.Create.SecretInKeyVault.ps1
+++ Powershell/04.Create.SecretInKeyVault.ps1
@@ -1,22 +1,13 @@
 #----------------------------------------------------------------------------------------------------------------------------
 # Setting up
-
-# Must run as Admin
-
-# Must log in some account
-Connect-AzureRmAccount
-# ----------------------------------------------------------------------------------------------------------------------------
-
-# ----------------------------------------------------------------------------------------------------------------------------
-# Geat&SetSubscriptionContextForAzureAccount
-# Find the subscriptions forthe account you are logged on
-Get-AzureRmSubscription
-
-# https://stackoverflow.com/questions/34928451/how-to-find-the-current-azure-rm-subscription
-# https://blogs.msdn.microsoft.com/benjaminperkins/2017/08/02/how-to-set-azure-powershell-to-a-specific-azure-subscription/
+Login-AzureRmAccount
 Get-AzureRmContext
 (Get-AzureRmContext).Subscription
 Set-AzureRmContext -SubscriptionId "df17c9fe-de76-4143-bbae-77b75fa0705b"
+Set-AzureRmContext -Subscription "df17c9fe-de76-4143-bbae-77b75fa0705b"
+Connect-AzureRmAccount
+# Find the subscriptions forthe account you are logged on
+Get-AzureRmSubscription
 # ----------------------------------------------------------------------------------------------------------------------------
 
 #----------------------------------------------------------------------------------------------------------------------------
