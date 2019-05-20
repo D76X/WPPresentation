91dc7599b662e2d49297865e197598c39f1b2dac
Davide Spano <davide.spano@wuerth-phoenix.com>
2019-05-20 17:32:09 +0200

    some code changes

---
+++ Powershell/00.FindCertificateByThumbprint&MmmsSnapin.ps1
@@ -0,0 +1,12 @@
+﻿#------------------------------------------------------------------------------------------------------------
+# Find a certificate via its thumbprint
+# http://geekswithblogs.net/JeremyMorgan/archive/2016/08/18/how-to-find-certificates-by-their-thumbprint.aspx
+cd CERT:\\
+Get-ChildItem -Path '792F7A2662626C57073A5748386D19D1037368EF' -Recurse
+#------------------------------------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------------------------------------
+# Manage Certificates Snap-in
+# https://superuser.com/questions/647036/view-install-certificates-for-local-machine-store-on-windows-7
+#------------------------------------------------------------------------------------------------------------
+
